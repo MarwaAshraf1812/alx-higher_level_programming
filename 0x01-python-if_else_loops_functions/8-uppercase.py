823eb3f03973ff51a3ec7240c1709533fef03bca
MarwaAshraf1812 <marashraf090@gmail.com>
2023-08-08 18:14:44 -0700

    isUpper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-def uppercase(s):
-    for char in s:
-        char_code = ord(char)
-        if ord('a') <= char_code <= ord('z'):
-            char_code -= 32
-        print(chr(char_code), end="")
-    print()
+def uppercase(str):
+    for ch in str:
+        if ord(ch) >= 97 and ord(ch) <= 122:
+            ch = chr(ord(ch) - 32)
+            print("{}".format(ch), end="")
+        print("")
