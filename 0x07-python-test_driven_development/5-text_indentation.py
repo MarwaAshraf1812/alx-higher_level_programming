8e3db4160e5069b8d7c42352c613ba3125782356
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-06 05:02:56 +0300

    Task : Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Defines a text-indentation function."""
+
+def text_indentation(text):
+    """
+        Print a text with two new lines after each '.', '?', and ':' character.
+
+        Args:
+        text (str): The input text to be formatted and printed.
+
+        Returns:
+        None: This function doesn't return a value; it prints the formatted text directly.
+    """
+    if not isinstance(text, str) or text is None:
+        raise TypeError('text must be a string')
+    
+    # lines = text.split("\n")
+    # for line in lines:
+    #     line = line.strip()
+    #     for s in line:
+    #         print(s, end="")
+    #         if s == '.' or s == '?' or s == ':':
+    #             print("\n")
+    #         elif s == '\n':
+    #             print("\n")
+    char = 0
+    while char < len(text) and text[char] == ' ':
+        char += 1
+
+    while char < len(text):
+        print(text[char], end="")
+        if text[char] == "\n" or text[char] in ".?:":
+            if text[char] in ".?:":
+                print("\n")
+            char += 1
+            while char < len(text) and text[char] == ' ':
+                char += 1
+            continue
+        char += 1
