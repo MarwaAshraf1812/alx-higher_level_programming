bb5b5eab1aed15b90311fb4b0ba9d28a52c5d260
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-06 05:05:59 +0300

    Task 0 : Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,6 +14,7 @@ def add_integer(a, b=98):
     TypeError : If either argument is not an Integer or float.
     return (a + b)
     """
+
     if not type(a) in (int, float):
         raise TypeError("a must be an integer")
     if  not type(b) in (int, float):
