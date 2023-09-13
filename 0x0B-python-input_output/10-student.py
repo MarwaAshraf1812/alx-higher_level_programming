341f8b4c3fbcd1955629a86b016efa638a9e806d
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-13 23:59:55 +0300

    Task 10 :  Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -18,4 +18,5 @@ class Student:
         """Retrieves a dictionary representation of the Student instance."""
         if (type(attrs) == list and
                 all(type(element) == str) for element in attrs):
-            return {i: getattr(self, attrs) for i in attrs}
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
