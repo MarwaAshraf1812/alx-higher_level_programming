6629b2529e4039a8370af4ad6a8d25052fa0a62b
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-14 02:51:53 +0300

    Task 12 :   Pascal's Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -10,7 +10,7 @@ def pascal_triangle(n):
         return []
     triangle = []
     for i in range(n + 1):
-            new = []
+        new = []
 
         for j in range(i):
             if j == 0 or j == i - 1:
