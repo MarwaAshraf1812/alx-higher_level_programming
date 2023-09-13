4af2628f2704a3ee8e00a966588b8dfb328c4174
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-13 23:02:57 +0300

    Task 8 : Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -7,6 +7,6 @@ for JSON serialization of an object
 """
 
 
-class_to_json(obj):
+def class_to_json(obj):
     """Returns the dictionary represntation of a simple data structure."""
     return obj.__dict__
