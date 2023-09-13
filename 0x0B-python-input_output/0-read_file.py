bf3faea330c7d8b728a9757d530cda8d3bd0bd14
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-13 05:53:07 +0300

    Task 0 :  Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,7 @@
 
 def read_file(filename=""):
     """""reads a text file(UTF8) and prints it to stdout"""
+    if filename == "":
+            return
     with open(filename, encoding="UTF8") as f:
             print(f.read(), end="")
