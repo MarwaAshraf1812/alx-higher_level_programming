b5fe0b91ab2b82c8c245ee93530e351c6aed5783
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-13 20:57:58 +0300

    Task 4 :  From JSON string to Object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,6 +5,6 @@ Contains the "from_json_string" fundtion
 import json
 
 
-def to_json_string(my_obj):
+def from_json_string(my_obj):
     """returns an object represented by a JSON string"""
     return json.loads(my_obj)
