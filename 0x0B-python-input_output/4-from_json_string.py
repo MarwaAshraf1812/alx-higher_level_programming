46c951a5153e73725428e5c6b7c9b8fb7039c414
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-13 06:35:59 +0300

    Task : From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Contains the "from_json_string" fundtion
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """returns an object represented by a JSON string"""
+    return json.loads(my_obj)
