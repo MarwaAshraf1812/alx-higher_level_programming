120e4f0e578ab318ef5e561debbb73c9e9885e53
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-19 01:33:12 +0300

    Square Model Update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square."""
-        super().__init__(width=size, height=size, x=x, y=y, id=id)
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
