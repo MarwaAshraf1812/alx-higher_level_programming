cbb8717c8970481519346cde14138d8b4858180d
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-19 02:05:49 +0300

    Square Model Update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,10 +8,8 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    Represent a square.
-    Inherits all attributes and methods of Rectangle
-    """
+    """Represent a square."""
+
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square."""
         super().__init__(size, size, x, y, id)
