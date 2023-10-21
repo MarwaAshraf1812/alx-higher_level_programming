ff130338191ebf281625e529c051cf6707bf504c
MarwaAshraf1812 <marashraf090@gmail.com>
2023-10-21 03:45:54 +0300

    Task 9: Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,2 @@
 -- Lsts all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, states.name
-    FROM cities LEFT JOIN states
-    ON cities.state_id = states.id
-    AND states.name = 'California'
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
