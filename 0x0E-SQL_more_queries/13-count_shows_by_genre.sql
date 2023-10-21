5b2fb2ac2660f18fd818a82181d2f6a4359d0975
MarwaAshraf1812 <marashraf090@gmail.com>
2023-10-21 04:55:06 +0300

    Task 13: Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,4 @@ FROM  tv_shows
 RIGHT JOIN tv_show_genres 
 ON tv_shows.id = tv_show_genres.show_id
 GROUP BY genre
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+ORDER BY number_of_shows DESC;
