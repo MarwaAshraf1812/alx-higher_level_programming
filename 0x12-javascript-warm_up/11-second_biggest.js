c418ce59b1201f4e12924f7aceb275741c943cd8
MarwaAshraf1812 <marashraf090@gmail.com>
2023-11-16 02:53:20 +0200

    Full advanced Updated

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,10 +2,9 @@
 
 const args = process.argv.slice(2).map(Number);
 
-if (args.length <= 1)
-{
-    console.log(0);
+if (args.length <= 1) {
+  console.log(0);
 } else {
-    const sortedNumbers = args.sort((a, b) => b - a);
-    console.log(sortedNumbers[1]);
+  const sortedNumbers = args.sort((a, b) => b - a);
+  console.log(sortedNumbers[1]);
 }
