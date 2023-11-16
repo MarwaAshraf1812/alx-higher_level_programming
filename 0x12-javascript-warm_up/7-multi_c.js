7e2c848846038156a53a8fb756559f02b50d6b6a
MarwaAshraf1812 <marashraf090@gmail.com>
2023-11-16 03:17:13 +0200

    L- Mandatory

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,13 +3,10 @@
 const args = process.argv;
 const num = parseInt(args[2]);
 
-if (!isNaN(num) && Number.isInteger(num))
-{
-    for (let i = 0; i < num; i++)
-    {
-        console.log('C is fun');
-    }
-}
-else{
-    console.log('Missing number of occurrences');
+if (!isNaN(num) && Number.isInteger(num)) {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
