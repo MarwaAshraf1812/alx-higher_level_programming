6ce3d52e48f44000c4d67dc23f05243671f27fce
MarwaAshraf1812 <marashraf090@gmail.com>
2023-12-13 01:49:37 +0200

    12. Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,20 +1,13 @@
 #!/usr/bin/node
 
 const dict = require('./101-data').dict;
-
-const valuesArray = Object.values(dict).sort((a, b) => a - b);
-
+const valuesArray = Object.values(dict);
+valuesArray.sort((a, b) => a - b);
 const keysByValue = {};
-
 for (const key in dict) {
-    const value = dict[key];
-
-    if (!keysByValue[value]) {
-        keysByValue[value] = [];
-    }
-
-    keysByValue[value].push(key);
+  const value = dict[key];
+  if (!keysByValue[value]) {
+    keysByValue[value] = [];
+  }
+  keysByValue[value].push(key);
 }
-
-console.log('Sorted Values:', valuesArray);
-console.log('Keys by Value:', keysByValue);
