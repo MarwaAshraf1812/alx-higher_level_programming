2ca0fe64b3966afabd41e4fde7d1e595526f4f2b
MarwaAshraf1812 <marashraf090@gmail.com>
2023-12-13 01:02:29 +0200

    6. Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,45 @@
+#!/usr/bin/node
+// const Rectangle = require('./4-rectangle');
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let i = 0;
+    const charPrint = c || 'X';
+    while (i < this.height) {
+      console.log(charPrint.repeat(this.width));
+      i++;
+    }
+  }
+}
+module.exports = Square;
