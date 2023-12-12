b8ece5fc5adb7cd0c83d7698ff74ce97232b4313
MarwaAshraf1812 <marashraf090@gmail.com>
2023-12-13 01:33:43 +0200

    11. Factor index

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+const valuesArray = Object.values(dict);
+valuesArray.sort((a, b) => a - b);
+keysByValue = {};
+for (const key in dict) {
+  const value = dict[key];
+  if (!keysByValue[value]) {
+    keysByValue[value] = [];
+  }
+  keysByValue[value].push(key);
+}
