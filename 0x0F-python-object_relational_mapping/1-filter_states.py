0ee2de89991f60d0e672831dd00655910dd0b4fc
MarwaAshraf1812 <marashraf090@gmail.com>
2023-12-20 15:08:15 +0200

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''
+Lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa.
+
+Usage:
+    ./1-filter_states.py <username> <password> <database>
+
+Parameters:
+    - <user>: MySQL database username.
+    - <password>: MySQL database password.
+    - <database>: Name of the MySQL database.
+
+Note: No argument validation needed.
+
+Example:
+    ./1-filter_states.py root root hbtn_0e_0_usa
+'''
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        mydb = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3]
+        )
+        mycursor = mydb.cursor()
+        mycursor.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
+        )
+
+        rows = mycursor.fetchall()
+
+        for row in rows:
+            print(row)
+        mydb.close()
