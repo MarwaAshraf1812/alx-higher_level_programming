fb33a1901f4fc57e54387a0d5531c027b99f4ec6
MarwaAshraf1812 <marashraf090@gmail.com>
2023-12-20 14:57:18 +0200

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 '''
-Prints all rows in the states table of a database.
+Lists all states from the database hbtn_0e_0_usa.
 
 Usage:
-    python script.py <username> <password> <database>
+    ./0-select_states.py <username> <password> <database>
 
 Parameters:
-    - <user>: MySQL database username.
+    - <username>: MySQL database username.
     - <password>: MySQL database password.
     - <database>: Name of the MySQL database.
 
+Note: No argument validation needed.
+
 Example:
-    python script.py myuser mypassword mydatabase
+    ./0-select_states.py root root hbtn_0e_0_usa
 '''
 
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    if len(sys.argv) >= 4:
+    if len(sys.argv) == 4:
         mydb = MySQLdb.connect(
             host='localhost',
             port=3306,
@@ -26,9 +28,11 @@ if __name__ == '__main__':
             password=sys.argv[2],
             database=sys.argv[3]
         )
-mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM states ORDER BY id ASC;")
-rows = mycursor.fetchall()
-for row in rows:
-    print(row)
-mydb.close()
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM states ORDER BY id ASC;")
+
+        rows = mycursor.fetchall()
+
+        for row in rows:
+            print(row)
+        mydb.close()
