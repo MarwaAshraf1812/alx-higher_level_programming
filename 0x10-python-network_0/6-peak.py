2b24bcdc925dbe2c8c6093c18195a818f0814098
MarwaAshraf1812 <marashraf090@gmail.com>
2024-01-26 18:54:00 +0200

    Task 6: Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Find a peak in a list of unsorted integers.
+
+    Returns:
+    - The peak element.
+    """
+
+
+def find_peak(list_of_integers):
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
+    else:
+        return None
