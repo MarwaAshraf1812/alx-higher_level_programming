c76014eca1c6c2dcf42199fb338d422478e4eca0
MarwaAshraf1812 <marashraf090@gmail.com>
2024-01-26 17:56:33 +0200

    Task 4: cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL, and displays the body of the response
-CUSTOM_H="X-HolbertonSchool-User-Id: 98"
-curl -s -H "CUSTOM_H" "${1}"
+curl -s -H "X-HolbertonSchool-User-Id: 98" "${1}"
