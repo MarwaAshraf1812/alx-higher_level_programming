443ec65bcbf96cbbcb762833c6376496ebf3dc1b
MarwaAshraf1812 <marashraf090@gmail.com>
2024-02-23 01:54:18 +0200

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,5 @@
 const fs = require('fs');
 const args = process.argv;
 fs.writeFile(args[2], args[3], 'utf-8', err => {
-  if (err) {
-    console.error(err);
-  }
+    if (err) console.log(err);
 });
