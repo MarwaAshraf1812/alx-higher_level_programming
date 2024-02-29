5e0346b6fc40ef12474dc0f3b568bca8de3c685b
MarwaAshraf1812 <marashraf090@gmail.com>
2024-02-29 12:23:07 +0200

    4

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,6 @@
+//toggles the class of the <header> element 
+//when the user clicks on the tag DIV#toggle_header
+
+$('div#red_header').click(function () {
+    $('header').ToggleClass('red');
+});
